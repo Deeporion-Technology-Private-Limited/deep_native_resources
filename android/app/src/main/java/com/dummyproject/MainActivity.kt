2cd00c697ba7f038f5fe91c163350ae4c89103a3
AjitBlr <ajit.sahu@deeporion.com>
2024-06-26 20:19:05 +0530

    modified project name

--- android/app/src/main/java/com/dummyproject/MainActivity.kt
+++ android/app/src/main/java/com/dummyproject/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.dummyproject
+package com.DoCommons
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "DummyProject"
+  override fun getMainComponentName(): String = "DoCommons"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
