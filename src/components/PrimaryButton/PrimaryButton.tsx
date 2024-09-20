42c2c53354b433d93cbeedb229ea294c37d8a09e
Devkrishn <krishn@Vighneshs-MacBook-Air.local>
2024-09-20 15:54:35 +0530

    updated Button name.

--- src/components/PrimaryButton/PrimaryButton.tsx
+++ src/components/PrimaryButton/PrimaryButton.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { TouchableOpacity, Text, StyleSheet, TouchableOpacityProps } from 'react-native';
 import styles from './style';
 
-interface MyButtonProps extends TouchableOpacityProps {
+interface PrimaryButtonProps extends TouchableOpacityProps {
   title: string;
 }
 
-const PrimaryButton: React.FC<MyButtonProps> = ({ title, style, ...rest }) => {
+const PrimaryButton: React.FC<PrimaryButtonProps> = ({ title, style, ...rest }) => {
   return (
     <TouchableOpacity style={[styles.button, style]} {...rest}>
       <Text style={styles.text}>{title}</Text>
