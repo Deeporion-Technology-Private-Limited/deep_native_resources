df8d11bf4de7942d5017dc50e0c11bc431edd048
AjitBlr <ajit.sahu@deeporion.com>
2024-09-23 13:57:49 +0530

    added toggle switch

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { default as SplashScreen } from './components/SplashScreen/SplashScreen'
 export { default as InputField } from './components/InputField/InputField';
 export { default as Checkbox } from './components/Checkbox/Checkbox';
 export { default as RadioButton } from './components/RadioButton/RadioButton';
+export { default as ToggleSwitch } from './components/Toggle/ToggleSwitch';
