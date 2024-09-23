2b110de0b6a8b462d97e876dedfa22682a02a294
AjitBlr <ajit.sahu@deeporion.com>
2024-09-23 13:31:10 +0530

    fix index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-export { default as MyButton } from './components/PrimaryButton/PrimaryButton';
+export { default as PrimaryButton } from './components/PrimaryButton/PrimaryButton';
 export { default as SplashScreen } from './components/SplashScreen/SplashScreen';
 export { default as InputField } from './components/InputField/InputField';
+export { default as Checkbox } from './components/Checkbox/Checkbox';
+export { default as RadioButton } from './components/RadioButton/RadioButton';
